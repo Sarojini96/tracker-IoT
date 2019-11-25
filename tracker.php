2015774d0449ffa313056ff8b1b91d4dbf97e2c0
sarojini <35261963+Sarojini96@users.noreply.github.com>
2019-11-25 19:23:11 +0530

    Add files via upload

---
+++ tracker.php
@@ -0,0 +1,23 @@
+<?php
+class gpsdata{
+ public $link='';
+ function __construct($userIP, $latitude, $longitude){
+  $this->connect();
+  $this->storeInDB($userIP, $latitude, $longitude);
+ }
+ 
+ function connect(){
+  $this->link = mysqli_connect('localhost','admin','admin123*','fishermantracker') or die('Cannot connect to the DB');
+  //mysqli_select_db($this->link,'<temphumidnew>') or die('Cannot select the DB');
+ }
+ 
+ function storeInDB($userIP, $latitude, $longitude){
+  $query = "insert into gpsdata set userIP='".$userIP."', latitude='".$latitude."',longitude='".$longitude."'";
+  $result = mysqli_query($this->link,$query) or die('Errant query:  '.$query);
+ }
+ 
+}
+if($_GET['userIP'] != ''and $_GET['latitude'] != '' and $_GET['longitude'] != ''){
+ $gpsdata=new gpsdata($_GET['userIP'],$_GET['latitude'],$_GET['longitude']);
+}
+?>
